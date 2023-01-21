58a3c09eb9a3e256be5e70943552d295063394a3
Maksim Ploski <50287455+plplmax@users.noreply.github.com>
2023-01-21 21:54:06 +0300

    test: create tests for `ScheduleNotification`

---
+++ app/src/test/java/com/github/plplmax/grsunotifications/notification/ScheduleNotificationTest.kt
@@ -0,0 +1,53 @@
+package com.github.plplmax.grsunotifications.notification
+
+import androidx.core.app.NotificationCompat
+import org.junit.Before
+import org.junit.Test
+import org.mockito.kotlin.*
+
+class ScheduleNotificationTest {
+    private lateinit var notification: ScheduleNotification
+    private val channel: NotificationChannel = mock()
+    private val builder: NotificationCompat.Builder = mock()
+    private val androidNotification: android.app.Notification = mock()
+
+    @Before
+    fun before() {
+        this.notification = ScheduleNotification(TITLE, TEXT)
+        whenever(channel.builder).thenReturn(builder)
+        whenever(builder.setContentTitle(any())).thenReturn(builder)
+        whenever(builder.setContentText(any())).thenReturn(builder)
+        whenever(builder.build()).thenReturn(androidNotification)
+    }
+
+    @Test
+    fun `when sending notification should use send`() {
+        notification.send(channel)
+
+        verify(channel).send(any())
+    }
+
+    @Test
+    fun `when sending notification should use notification builder`() {
+        notification.send(channel)
+
+        builder.inOrder {
+            verify().setContentTitle(eq(TITLE))
+            verify().setContentText(eq(TEXT))
+            verify().build()
+            Unit
+        }
+    }
+
+    @Test
+    fun `when sending notification should send notification from builder`() {
+        notification.send(channel)
+
+        verify(channel).send(androidNotification)
+    }
+
+    companion object {
+        private const val TITLE = "TITLE"
+        private const val TEXT = "TEXT"
+    }
+}
