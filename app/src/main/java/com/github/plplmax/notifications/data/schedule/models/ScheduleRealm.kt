eccfe316919ae4f40902d3a2f5a46774e0fb0b01
Maksim Ploski <50287455+plplmax@users.noreply.github.com>
2023-06-18 07:50:02 +0300

    fix: add the @PrimaryKey annotation

--- app/src/main/java/com/github/plplmax/notifications/data/schedule/models/ScheduleRealm.kt
+++ app/src/main/java/com/github/plplmax/notifications/data/schedule/models/ScheduleRealm.kt
@@ -2,9 +2,11 @@ package com.github.plplmax.notifications.data.schedule.models
 
 import io.realm.RealmList
 import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
 import org.bson.types.ObjectId
 
 open class ScheduleRealm : RealmObject() {
+    @PrimaryKey
     var id: ObjectId = ObjectId()
     var days: RealmList<DayRealm> = RealmList()
 }
