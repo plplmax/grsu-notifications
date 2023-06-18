2021faac038b4be697a08b46daa9310b6437309b
Maksim Ploski <50287455+plplmax@users.noreply.github.com>
2023-06-18 03:56:57 +0300

    feat: connect TabRow with Pager

--- app/src/main/java/com/github/plplmax/notifications/ui/diff/DiffScreen.kt
+++ app/src/main/java/com/github/plplmax/notifications/ui/diff/DiffScreen.kt
@@ -10,8 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Card
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
@@ -21,27 +20,34 @@ import androidx.compose.material3.Tab
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.github.plplmax.notifications.App
+import com.github.plplmax.notifications.MainActivity
 import com.github.plplmax.notifications.data.schedule.models.Day
 import com.github.plplmax.notifications.data.schedule.models.DiffedSchedule
 import com.github.plplmax.notifications.data.schedule.models.Lesson
 import com.github.plplmax.notifications.data.schedule.models.Teacher
 import com.github.plplmax.notifications.ui.theme.GrsuNotificationsTheme
+import kotlinx.coroutines.launch
 
 @Composable
-fun DiffScreen(viewModel: DiffViewModel = viewModel()) {
-    // @todo move id to the function's parameters
-    val id = "test-id"
+fun DiffScreen(id: String) {
+    val context = LocalContext.current
+    val app = remember(context) {
+        ((context as MainActivity).application) as App
+    }
+    val viewModel = viewModel(initializer = {
+        DiffViewModel(app.deps.diffedScheduleRepository)
+    })
     LaunchedEffect(id) {
         viewModel.loadScheduleById(id)
     }
@@ -56,37 +62,34 @@ fun DiffScreen(viewModel: DiffViewModel = viewModel()) {
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun DiffContent(schedule: DiffedSchedule) {
-    // @todo make synchronization between tabs and pager
-    var selectedTab by remember { mutableStateOf(0) }
+    if (schedule.days.isEmpty()) {
+        Text(text = "There are no differences")
+        return
+    }
+    val pagerState = rememberPagerState()
+    val scope = rememberCoroutineScope()
     Column {
         ScrollableTabRow(
-            selectedTabIndex = selectedTab,
-            edgePadding = 24.dp,
-            modifier = Modifier.padding(14.dp)
+            selectedTabIndex = pagerState.currentPage,
+            edgePadding = 24.dp
         ) {
-            Tab(selected = 0 == selectedTab, onClick = { selectedTab = 0 }) {
-                Text(text = "Test")
-            }
-            Tab(selected = 1 == selectedTab, onClick = { selectedTab = 1 }) {
-                Text(text = "Test")
-            }
-            Tab(selected = 2 == selectedTab, onClick = { selectedTab = 2 }) {
-                Text(text = "Test")
+            schedule.days.forEachIndexed { index, day ->
+                Tab(
+                    selected = pagerState.currentPage == index,
+                    onClick = { scope.launch { pagerState.animateScrollToPage(index) } }
+                ) {
+                    Text(text = day.date, modifier = Modifier.padding(14.dp))
+                }
             }
         }
         HorizontalPager(
             pageCount = schedule.days.size,
             contentPadding = PaddingValues(14.dp),
             pageSpacing = 4.dp,
-            modifier = Modifier.verticalScroll(rememberScrollState())
+            state = pagerState
         ) { index ->
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-            ) {
-                schedule.days[index].lessons.forEach {
-                    ScheduleCard(it)
-                }
+            Column(modifier = Modifier.fillMaxSize()) {
+                schedule.days[index].lessons.forEach { ScheduleCard(it) }
             }
         }
     }
@@ -143,7 +146,7 @@ fun ScheduleCard(lesson: Lesson) {
 
 @Composable
 fun DiffText(text: String, style: TextStyle = LocalTextStyle.current) {
-    val color = when (text.first()) {
+    val color = when (text.firstOrNull()) {
         '+' -> {
             Color.Green.copy(alpha = 0.2f)
         }
