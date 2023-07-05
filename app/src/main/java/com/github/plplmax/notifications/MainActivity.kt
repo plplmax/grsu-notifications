a10a2720e7a070ec92f3b568ef9b3fc165e30de8
Maksim Ploski <50287455+plplmax@users.noreply.github.com>
2023-07-05 12:10:29 +0300

    refactor: remove redundant imports

--- app/src/main/java/com/github/plplmax/notifications/MainActivity.kt
+++ app/src/main/java/com/github/plplmax/notifications/MainActivity.kt
@@ -1,49 +1,18 @@
 package com.github.plplmax.notifications
 
-import android.Manifest
-import android.annotation.SuppressLint
-import android.content.Context
-import android.content.Intent
-import android.net.Uri
-import android.os.Build
 import android.os.Bundle
-import android.provider.Settings
-import android.provider.Settings.EXTRA_APP_PACKAGE
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
-import androidx.compose.animation.animateContentSize
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.Alignment
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.core.content.ContextCompat.startActivity
 import androidx.work.WorkManager
-import com.github.plplmax.notifications.data.Constants
 import com.github.plplmax.notifications.ui.login.LoginScreen
 import com.github.plplmax.notifications.ui.theme.GrsuNotificationsTheme
-import com.github.plplmax.notifications.ui.welcome.WelcomeScreen
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
-import java.text.Normalizer
-import kotlin.contracts.ExperimentalContracts
-import kotlin.contracts.contract
 
 class MainActivity : ComponentActivity() {
     private val viewModel: MainViewModel by viewModels(factoryProducer = {
