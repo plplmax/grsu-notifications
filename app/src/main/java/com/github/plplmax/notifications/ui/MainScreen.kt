1db15d4151bede117178089fa727c187db006776
Maksim Ploski <50287455+plplmax@users.noreply.github.com>
2023-07-24 19:17:24 +0300

    feat: implement composition local for SnackbarHostState

--- app/src/main/java/com/github/plplmax/notifications/ui/MainScreen.kt
+++ app/src/main/java/com/github/plplmax/notifications/ui/MainScreen.kt
@@ -2,14 +2,16 @@ package com.github.plplmax.notifications.ui
 
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.github.plplmax.notifications.MainViewModel
 import com.github.plplmax.notifications.ui.navigation.AppNavHost
+import com.github.plplmax.notifications.ui.snackbar.LocalSnackbarState
 
 @Composable
 fun MainScreen(viewModel: MainViewModel) {
-    Scaffold { padding ->
+    Scaffold(snackbarHost = { SnackbarHost(hostState = LocalSnackbarState.current) }) { padding ->
         AppNavHost(
             viewModel = viewModel,
             modifier = Modifier.padding(padding)
