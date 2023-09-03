b4e16d2d00e09f0ba2ace827293c603140b5ba0c
Maksim Ploski <50287455+plplmax@users.noreply.github.com>
2023-09-03 14:35:20 +0300

    feat: change schedule update window to the one week (#59)

--- app/src/main/java/com/github/plplmax/notifications/window/ScheduleUpdateWindowOf.kt
+++ app/src/main/java/com/github/plplmax/notifications/window/ScheduleUpdateWindowOf.kt
@@ -1,16 +1,11 @@
 package com.github.plplmax.notifications.window
 
-import java.time.DayOfWeek
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
-import java.time.temporal.ChronoField
 
-class ScheduleUpdateWindowOf(private val date: LocalDate) : ScheduleUpdateWindow {
-    private val startDate: LocalDate by lazy {
-        date.with(ChronoField.DAY_OF_WEEK, DayOfWeek.MONDAY.value.toLong())
-    }
-
-    private val endDate: LocalDate by lazy { startDate.plusDays(9) }
+class ScheduleUpdateWindowOf(date: LocalDate) : ScheduleUpdateWindow {
+    private val startDate: LocalDate = date
+    private val endDate: LocalDate by lazy { startDate.plusDays(6) }
 
     override fun startDateAsString(formatter: DateTimeFormatter): String {
         return formatter.format(startDate)
