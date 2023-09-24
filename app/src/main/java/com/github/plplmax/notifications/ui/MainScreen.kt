9c238f3561c256656cffde22577a6d636133bf0b
Maksim Ploski <50287455+plplmax@users.noreply.github.com>
2023-09-24 17:22:04 +0300

    refactor: improve readability

--- app/src/main/java/com/github/plplmax/notifications/ui/MainScreen.kt
+++ app/src/main/java/com/github/plplmax/notifications/ui/MainScreen.kt
@@ -2,25 +2,16 @@ package com.github.plplmax.notifications.ui
 
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.DrawerValue
-import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.github.plplmax.notifications.MainViewModel
-import com.github.plplmax.notifications.R
-import com.github.plplmax.notifications.UiState
 import com.github.plplmax.notifications.ui.navigation.AppNavHost
-import com.github.plplmax.notifications.ui.navigation.NavigationDrawerContent
-import com.github.plplmax.notifications.ui.navigation.Routes
+import com.github.plplmax.notifications.ui.navigation.AppNavigationDrawer
 import com.github.plplmax.notifications.ui.snackbar.LocalSnackbarState
 import kotlinx.coroutines.launch
 
@@ -29,38 +20,10 @@ fun MainScreen(viewModel: MainViewModel) {
     val navController = rememberNavController()
     val scope = rememberCoroutineScope()
     val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
-    val backStack by navController.currentBackStackEntryAsState()
-    val gesturesEnabled by remember {
-        derivedStateOf { backStack?.destination?.route == Routes.Notifications.route }
-    }
-    val snackbarState = LocalSnackbarState.current
-    val context = LocalContext.current
-    ModalNavigationDrawer(
-        drawerContent = {
-            NavigationDrawerContent(
-                login = (viewModel.state as? UiState.Success)?.login ?: "",
-                currentRoute = backStack?.destination?.route ?: "",
-                openNotifications = {
-                    scope.launch {
-                        navController.navigate(Routes.Notifications.route) {
-                            launchSingleTop = true
-                        }
-                        drawerState.close()
-                    }
-                },
-                onSignOut = {
-                    scope.launch {
-                        val signedOut = viewModel.signOut()
-                        drawerState.close()
-                        if (!signedOut.await()) {
-                            snackbarState.showSnackbar(context.getString(R.string.something_went_wrong))
-                        }
-                    }
-                }
-            )
-        },
-        drawerState = drawerState,
-        gesturesEnabled = gesturesEnabled
+    AppNavigationDrawer(
+        viewModel = viewModel,
+        navController = navController,
+        drawerState = drawerState
     ) {
         Scaffold(
             snackbarHost = { SnackbarHost(hostState = LocalSnackbarState.current) }
--- app/src/main/java/com/github/plplmax/notifications/ui/navigation/AppNavigationDrawer.kt
+++ app/src/main/java/com/github/plplmax/notifications/ui/navigation/AppNavigationDrawer.kt
@@ -7,25 +7,81 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ExitToApp
 import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material3.Divider
+import androidx.compose.material3.DrawerState
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import com.github.plplmax.notifications.MainViewModel
 import com.github.plplmax.notifications.R
+import com.github.plplmax.notifications.UiState
 import com.github.plplmax.notifications.ui.alert.ProblemWithNotificationsDialog
+import com.github.plplmax.notifications.ui.snackbar.LocalSnackbarState
+import kotlinx.coroutines.launch
 
 @Composable
-fun NavigationDrawerContent(
+fun AppNavigationDrawer(
+    viewModel: MainViewModel,
+    navController: NavController,
+    drawerState: DrawerState,
+    content: @Composable () -> Unit
+) {
+    val scope = rememberCoroutineScope()
+    val backStack by navController.currentBackStackEntryAsState()
+    val gesturesEnabled by remember {
+        derivedStateOf { backStack?.destination?.route == Routes.Notifications.route }
+    }
+    val snackbarState = LocalSnackbarState.current
+    val context = LocalContext.current
+    ModalNavigationDrawer(
+        drawerContent = {
+            DrawerContent(
+                login = (viewModel.state as? UiState.Success)?.login ?: "",
+                currentRoute = backStack?.destination?.route ?: "",
+                openNotifications = {
+                    scope.launch {
+                        navController.navigate(Routes.Notifications.route) {
+                            launchSingleTop = true
+                        }
+                        drawerState.close()
+                    }
+                },
+                onSignOut = {
+                    scope.launch {
+                        val signedOut = viewModel.signOut()
+                        drawerState.close()
+                        if (!signedOut.await()) {
+                            val message = context.getString(R.string.something_went_wrong)
+                            snackbarState.showSnackbar(message)
+                        }
+                    }
+                }
+            )
+        },
+        drawerState = drawerState,
+        gesturesEnabled = gesturesEnabled,
+        content = content
+    )
+}
+
+@Composable
+private fun DrawerContent(
     login: String,
     currentRoute: String,
     openNotifications: () -> Unit,
