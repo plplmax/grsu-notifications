7f63ef0ead6abd74ccd78505b6b3e1985d80fcc7
Maksim Ploski <50287455+plplmax@users.noreply.github.com>
2023-11-13 02:49:04 +0300

    feat: improve color schemes (#131)

--- app/src/main/java/com/github/plplmax/notifications/ui/theme/Color.kt
+++ app/src/main/java/com/github/plplmax/notifications/ui/theme/Color.kt
@@ -2,10 +2,6 @@ package com.github.plplmax.notifications.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Blue40 = Color(0xFF435E91)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
 val Blue80 = Color(0xFFACC7FF)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Blue40 = Color(0xFF2F5DA8)
+val Blue20 = Color(0xFF002F68)
--- app/src/main/java/com/github/plplmax/notifications/ui/theme/Theme.kt
+++ app/src/main/java/com/github/plplmax/notifications/ui/theme/Theme.kt
@@ -2,35 +2,27 @@ package com.github.plplmax.notifications.ui.theme
 
 import android.app.Activity
 import android.os.Build
-import androidx.compose.material3.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
-import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = Blue80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    onPrimary = Blue20
 )
 
 private val LightColorScheme = lightColorScheme(
     primary = Blue40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    onPrimary = Color.White
 )
 
 @Composable
