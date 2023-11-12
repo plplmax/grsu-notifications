99f37bcd2bc6326d769e4081520f7d30fff10dcd
Maksim Ploski <50287455+plplmax@users.noreply.github.com>
2023-11-13 02:53:08 +0300

    feat: change status bar color (#133)

--- app/src/main/java/com/github/plplmax/notifications/ui/theme/Theme.kt
+++ app/src/main/java/com/github/plplmax/notifications/ui/theme/Theme.kt
@@ -44,8 +44,8 @@ fun GrsuNotificationsTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            window.statusBarColor = colorScheme.background.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
 
